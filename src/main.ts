dceb4e84fd31731d6c814311c9bb2e40d7e95749
ArrudaFreitas <ianarruda7@gmail.com>
2026-03-22 23:07:47 -0300

    chore(ui): configura PrimeVue Aura

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
 import './assets/main.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura
+  }
+})
+
+app.mount('#app')
