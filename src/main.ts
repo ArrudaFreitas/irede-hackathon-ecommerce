84a67e86823d4ac4c311d6ea6565af08054659da
ArrudaFreitas <ianarruda7@gmail.com>
2026-03-24 00:14:10 -0300

    chore(main): registra Pinia na aplicação

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import PrimeVue from 'primevue/config'
 import Aura from '@primevue/themes/aura'
@@ -7,6 +8,7 @@ import './assets/main.css'
 
 const app = createApp(App)
 
+app.use(createPinia())
 app.use(router)
 app.use(PrimeVue, {
   theme: {
