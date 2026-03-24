e61e3fa34ca09762b3c2741c7824b94287ee96ea
Gustavo Erick <gustavoerickv@gmail.com>
2026-03-24 16:13:42 -0300

    fix(banner): altera o componente `HomeBanner` e corrige o tamanho da imagem

--- src/components/HomeBanner.vue
+++ src/components/HomeBanner.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import Galleria from "primevue/galleria";
-
 import banner1 from "@/assets/banners/banner-1.png";
 import banner2 from "@/assets/banners/banner-2.png";
 import banner3 from "@/assets/banners/banner-3.jpg";
@@ -22,29 +20,16 @@ const banners = [
 </script>
 
 <template>
-  <section class="w-full">
-    <Galleria
-      :value="banners"
-      :numVisible="5"
-      :circular="true"
-      :autoPlay="true"
-      :transitionInterval="5000"
-      :showThumbnails="false"
-      :showIndicators="true"
-      :showItemNavigators="true"
-      containerClass="w-full"
-    >
-      <template #item="{ item }">
-        <div
-          class="w-full h-[220px] sm:h-[320px] md:h-[420px] lg:h-[520px] overflow-hidden"
-        >
-          <img
-            :src="item.image"
-            :alt="item.alt"
-            class="w-full h-full object-cover"
-          />
-        </div>
-      </template>
-    </Galleria>
-  </section>
+  <Carousel
+    :value="banners"
+    :num-visible="1"
+    :num-scroll="1"
+    :autoplay-interval="4000"
+    circular
+    class="w-full"
+  >
+    <template #item="{ data }">
+      <img :src="data.image" :alt="data.alt" class="w-full block" />
+    </template>
+  </Carousel>
 </template>
