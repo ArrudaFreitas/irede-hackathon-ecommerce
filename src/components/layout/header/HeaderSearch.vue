23ae3ce3c597ed3b846d65238a3a89d542b34e40
ArrudaFreitas <ianarruda7@gmail.com>
2026-03-25 16:43:01 -0300

    fix(header-search): correção para a busca corresponder a query

--- src/components/layout/header/HeaderSearch.vue
+++ src/components/layout/header/HeaderSearch.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts">
-import { ref } from "vue";
-import { useRouter } from "vue-router";
-
+import { ref, watch } from "vue";
+import { useRouter, useRoute } from "vue-router";
 import IconField from "primevue/iconfield";
 import InputIcon from "primevue/inputicon";
 import InputText from "primevue/inputtext";
-
 import { Icon } from "@iconify/vue";
 
 const props = defineProps<{
@@ -14,15 +12,23 @@ const props = defineProps<{
 }>();
 
 const router = useRouter();
-const searchQuery = ref("");
+const route = useRoute();
+
+const searchQuery = ref((route.query.q as string) || "");
+
+watch(
+    () => route.query.q,
+    (newQ) => {
+        searchQuery.value = (newQ as string) || "";
+    }
+);
 
 function search() {
     const q = searchQuery.value.trim();
     if (!q) return;
-
     router.push({
         name: "products",
-        query: { q }
+        query: { q },
     });
 }
 </script>
@@ -32,7 +38,6 @@ function search() {
         <InputIcon>
             <Icon icon="mdi:magnify" />
         </InputIcon>
-
         <InputText v-model="searchQuery" :placeholder="placeholder || 'Buscar produtos...'" class="w-full" :size="size"
             @keyup.enter="search" />
     </IconField>
