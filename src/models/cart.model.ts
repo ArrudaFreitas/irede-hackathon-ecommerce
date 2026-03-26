08382741183965436956c6001a375f37b5dbffa1
ArrudaFreitas <ianarruda7@gmail.com>
2026-03-26 19:49:54 -0300

    chore(models): adiciona model de cart e cartItem

---
+++ src/models/cart.model.ts
@@ -0,0 +1,37 @@
+export interface CartProduct {
+    id: number
+    title: string
+    price: number
+    quantity: number
+    total: number
+    discountPercentage: number
+    discountedTotal: number
+    thumbnail: string
+}
+
+export interface Cart {
+    id: number
+    products: CartProduct[]
+    total: number
+    discountedTotal: number
+    userId: number
+    totalProducts: number
+    totalQuantity: number
+}
+
+export interface CartsResponse {
+    carts: Cart[]
+    total: number
+    skip: number
+    limit: number
+}
+
+export interface CartItem {
+    id: number
+    title: string
+    price: number
+    discountPercentage: number
+    discountedPrice: number
+    quantity: number
+    thumbnail: string
+}
